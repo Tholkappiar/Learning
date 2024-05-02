b0d95ff6d840fad78f05c838947b8b88222e94d1
tholkappiar <tholkappiar2003@gmail.com>
2024-05-02 22:39:00 +0530

    Arrays: Largest sub Array with 0 sum

---
+++ DSA/LeetCode/Arrays/LargestSubArrayWith0Sum.java
@@ -0,0 +1,18 @@
+int maxLen(int[] arr, int n) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int maxLen = 0;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+        if (sum == 0)
+            maxLen = i + 1;
+        else {
+            if (map.containsKey(sum))
+                maxLen = Math.max(maxLen, i - map.get(sum));
+            else
+                map.put(sum, i);
+        }
+    }
+    return maxLen;
+}
